16b0e83d1861e3db4071d29e59155bbf993dd756
hananeikhelef <hananeikhelef@gmail.com>
2020-06-05 12:38:33 +0000

    Add comments

--- portfolio/src/main/java/com/google/sps/data/Message.java
+++ portfolio/src/main/java/com/google/sps/data/Message.java
@@ -1,14 +1,17 @@
 package com.google.sps.data;
 
-
-/** A message in the comment section */
-
+/** A message to the comment section. */
 public final class  Message{
 
   private final long id;
   private final String content;
   private final long timestamp;
 
+   /**
+   * @param id         datastore-generated unique id for this comment.
+   * @param content   main text of this message.
+   * @param timestamp  comment creation date.
+   */
   public Message(long id, String content, long timestamp) {
     this.id = id;
     this.content = content;
