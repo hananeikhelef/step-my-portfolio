c02cbd3788cde3cbd6bc3c4e02cc8392f12dd4a8
hananeikhelef <hananeikhelef@gmail.com>
2020-06-15 16:26:47 +0000

    Add maps Library

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -76,7 +76,15 @@ async function comment() {
     }
 }
 
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
 window.onload = function(){
     getData();
     comment();
+    createMap();
 }
