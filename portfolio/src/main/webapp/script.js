046d04ce5826f15d58e3a3b45b75fe340a8e239f
hananeikhelef <hananeikhelef@gmail.com>
2020-06-15 17:42:15 +0000

    Add map funciton

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -59,3 +59,16 @@ function deleteMessage(message) {
   params.append('id', message);
   fetch('/delete-data', {method: 'POST', body: params});
 }
+
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
+window.onload = function(){
+    getData();
+    comment();
+    createMap();
+}
